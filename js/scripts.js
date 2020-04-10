e8a2a88ff843e5afdc0b6e9e7a2c2afa0832c0ba
Daniel Merys <dkmerys@gmail.com>
2020-04-10 10:47:16 -0700

    remove extraneous formatting in HTML

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,8 @@ function Pizza(size, toppings) {
   this.toppings = toppings
 };
 
-var toppings = ["pepperoni", "sausage", "mushrooms", "bacon", "black-olives", "canadian-bacon", "chicken", "extra-cheese", "anchovies", "bell-peppers"];
-var size = ["small", "medium", "large", "family"]
-var userPizza = new Pizza (size, toppings)
+// var toppings = 
+// var userPizza = new Pizza (size, toppings)
 // write a prototype method that uses the properties of the object
 // to determine the price and adds price as a new property to the object
 
@@ -26,7 +25,9 @@ Pizza.prototype.sizeCheck = function() {
 // return final price to user via html
 
 
+Pizza.prototype.sizeCheck = function() {
 
+}
 
 
 
@@ -37,8 +38,29 @@ Pizza.prototype.sizeCheck = function() {
 
 
 // UI Logic
+
+var sizeCheck = function(size) {
+  if (size === "small") {
+    this.price = 12
+  }
+}
+
 $(document).ready(function() {
   $("form#menu").submit(function(event) {
-    $(event).preventDefault();
+    event.preventDefault();
+    $("input:checkbox[name=topping]:checked").each.click(function(){
+      if($(this).prop("checked") == true){
+        console.log("checkbox is checked.")
+      }
+      else if($(this).prop("checked") ==false){
+        console.log("Checkbox is unchecked.");
+      }
+    })
+
+    // var size = $("input:radio[name=size]:checked").val()
+    // console.log(size)
+
+    // var toppings = $("input:checkbox[name=topping]:checked").val()
+    // console.log(toppings)
   })
 });
