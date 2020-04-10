d03e271e2711dda1c0dd585fb99b62106ada14c1
Daniel Merys <dkmerys@gmail.com>
2020-04-10 08:49:47 -0700

    add basic HTML structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
+// Business Logic
+function Pizza(size, toppings) {
+  this.size = [small, medium, large, family]
+  this.toppings = [pepperoni, sausage, mushrooms, bacon, black-olives, canadian-bacon, chicken, extra-cheese, anchovies, bell-peppers];
+};
+
+// write a prototype method that uses the properties of the object
+// to determine the price and adds price as a new property to the object
+
+// Pizza.prototype.calculate = function() {
+//   this.price = 12
+
+//   if (this.size === small)
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// UI Logic
 $(document).ready(function() {
 
 });
