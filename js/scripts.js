f67112ea3606afcc141bbaae9b114a26951745d9
Daniel Merys <dkmerys@gmail.com>
2020-04-10 09:25:10 -0700

    update radio button labels

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,29 @@
 // Business Logic
 function Pizza(size, toppings) {
-  this.size = [small, medium, large, family]
-  this.toppings = [pepperoni, sausage, mushrooms, bacon, black-olives, canadian-bacon, chicken, extra-cheese, anchovies, bell-peppers];
+  this.size = size
+  this.toppings = toppings
 };
 
+var toppings = ["pepperoni", "sausage", "mushrooms", "bacon", "black-olives", "canadian-bacon", "chicken", "extra-cheese", "anchovies", "bell-peppers"];
+var size = ["small", "medium", "large", "family"]
+var userPizza = new Pizza (size, toppings)
 // write a prototype method that uses the properties of the object
 // to determine the price and adds price as a new property to the object
 
-// Pizza.prototype.calculate = function() {
-//   this.price = 12
 
-//   if (this.size === small)
-// };
+Pizza.prototype.sizeCheck = function() {
 
+}
+// on submit, check which radio button from .size div is checked,
+// pass that to size variable
+// which one determines base price
 
+// on submit, check how many radio buttons from .toppings div are checked
+// pass that to toppings variable
+// multiply number of toppings checked by 1.5
+// add that number to base price to get final price
+
+// return final price to user via html
 
 
 
@@ -28,5 +38,7 @@ function Pizza(size, toppings) {
 
 // UI Logic
 $(document).ready(function() {
-
+  $("form#menu").submit(function(event) {
+    $(event).preventDefault();
+  })
 });
